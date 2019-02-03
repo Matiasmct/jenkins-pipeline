049db2da8222dea85e4c23331cb974a4e6a725d6
Matias Costa <mateomct@gmail.com>
2019-02-03 14:27:10 +0000

    Fix if condition if chart version is not specified

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -32,7 +32,7 @@ def helmConfig(tiller, helm_repo) {
 def helmDeploy(Map args) {
 
     println "Running deployment"
-    if (args.chart_version == null) {
+    if (args.chart_version == "null") {
         sh "export AWS_REGION=\'eu-west-1\' && helm upgrade --install ${args.namespace}-${args.appname} ${args.helm_repo}/${args.appname} -f ${args.namespace}/${args.environment}/values.yaml --namespace=${args.namespace} --tiller-namespace=${args.tiller}"
         echo "Application ${args.appname} latest version successfully deployed. Use helm status ${args.appname} to check"
     } else {
